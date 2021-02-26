70f2830a91a5a09e29fce03c589c11f9a5c4eabc
mksain <mkcomputeredu03@gmail.com>
2021-02-26 23:17:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div bg-success">
+      <div className="row justify-content-center bg-danger">
+        <div className="col-5 bg-secondary">
+          <div className="row">
+            <div className="col pt-3 header">
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" style={{ "background-color": "#b13c3cfa" }} />
+                <button className="btn btn-outline-secondary" type="button" id="button-addon2"> + </button>
+              </div>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item">An item</li>
+              <li class="list-group-item">A second item</li>
+              <li class="list-group-item">A third item</li>
+              <li class="list-group-item">A fourth item</li>
+              <li class="list-group-item">And a fifth one</li>
+            </ul>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
