2bc4de05dbb104d10243332c11672a2cb918bf6d
mksain <mkcomputeredu03@gmail.com>
2021-02-27 11:54:40 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from "react";
+import List from "./List";
 function App() {
+
+const [inputList,setinputList]=useState("");
+
+const [Items,setItems]=useState([]);
+
+const itemEvent = (event)=>
+{
+    setinputList(event.target.value);
+};
+
+const listofItems=()=>
+{
+    setItems((oldItems)=>{
+      return [...oldItems,inputList];
+    });
+    setinputList('');
+};
+
   return (
     <div className="main_div bg-success">
-      <div className="row justify-content-center bg-danger">
-        <div className="col-5 bg-secondary">
+      <div className="row justify-content-center">
+        <div className="col-5">
           <div className="row">
             <div className="col pt-3 header">
+              <div className="sub-header" style={{background:"#b13c3cfa"}}>
               <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" style={{ "background-color": "#b13c3cfa" }} />
-                <button className="btn btn-outline-secondary" type="button" id="button-addon2"> + </button>
+                <input type="text" value={inputList} onChange={itemEvent} className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" style={{ "background-color": "#b13c3cfa" }} />
+                <button onClick={listofItems} className="btn btn-outline-secondary" type="button" id="button-addon2"> + </button>
+              </div>
               </div>
             </div>
             <ul class="list-group">
-              <li class="list-group-item">An item</li>
-              <li class="list-group-item">A second item</li>
-              <li class="list-group-item">A third item</li>
-              <li class="list-group-item">A fourth item</li>
-              <li class="list-group-item">And a fifth one</li>
+              {Items.map((itemval) =>{
+                return <List text={itemval}/>;
+              })}
+              
+              
             </ul>
           </div>
         </div>
---
+++ src/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const List=(props)=>{
+
+    return <li class="list-group-item">{props.text}</li>;
+};
+
+export default List;
