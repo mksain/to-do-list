cb8eb2efc1d3f88aa5df865faf762aa661976a87
mksain <mkcomputeredu03@gmail.com>
2021-02-27 19:47:41 +0530

    submit1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{useState} from "react";
 import List from "./List";
@@ -21,22 +20,37 @@ const listofItems=()=>
     setinputList('');
 };
 
+const deleteItems =(id)=>{
+  console.log("deleted");
+
+  setItems((oldItems)=>{
+    return oldItems.filter((arrElement,index)=>{
+      return index !== id;
+    })
+  })
+}
+
   return (
-    <div className="main_div bg-success">
+    <div className="main_div">
       <div className="row justify-content-center">
         <div className="col-5">
           <div className="row">
             <div className="col pt-3 header">
               <div className="sub-header" style={{background:"#b13c3cfa"}}>
               <div className="input-group mb-3">
-                <input type="text" value={inputList} onChange={itemEvent} className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" style={{ "background-color": "#b13c3cfa" }} />
+                <input type="text" value={inputList} onChange={itemEvent} className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" style={{ "backgroundColor": "#b13c3cfa" }} />
                 <button onClick={listofItems} className="btn btn-outline-secondary" type="button" id="button-addon2"> + </button>
               </div>
               </div>
             </div>
-            <ul class="list-group">
-              {Items.map((itemval) =>{
-                return <List text={itemval}/>;
+            <ul className="list-group">
+              {Items.map((itemval,index) =>{
+                return <List 
+                key={index}
+                 text={itemval}
+                 id={index}
+                 onSelect={deleteItems}
+                 />;
               })}
               
               
--- src/List.js
+++ src/List.js
@@ -1,8 +1,18 @@
 import React from "react";
-
+import './App.css';
 const List=(props)=>{
 
-    return <li class="list-group-item">{props.text}</li>;
+
+    return (
+    <div className="list-item">
+        <li className="list-group-item">{props.text}</li>
+        <span><i className="far fa-trash-alt"
+         onClick={()=>{
+           props.onSelect(props.id);
+        }} ></i></span>
+    </div>
+   
+    );
 };
 
 export default List;
